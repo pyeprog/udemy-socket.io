aa7fa2601bc0cd055aae23377d1a8facf091bc08
pd <pd@pds-MacBook-Pro.local>
2018-01-16 20:23:03 +0800

    Add geolocation

--- public/js/index.js
+++ public/js/index.js
@@ -10,5 +10,47 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (msg) {
   console.log('newMessage', msg);
+  var li = jQuery('<li></li>');
+  li.text(`${msg.from}: ${msg.text}`)
+
+  jQuery('#messages').append(li);
 });
 
+socket.on('newLocationMessage', function(msg) {
+  var li = jQuery('<li></li>');
+  var a = jQuery('<a target="_blank">My current location</a>')
+
+  li.text(`${msg.from}: `);
+  a.attr('href', msg.url); 
+  li.append(a);
+
+  jQuery('#messages').append(li);
+});
+
+jQuery('#message-form').on('submit', function(e) {
+  e.preventDefault();
+
+  socket.emit('createMessage', {
+    from: 'User',
+    text: jQuery('[name=message]').val()
+  }, function() {
+    console.log('msg get');
+  })
+})
+
+var locationButton = jQuery('#send-location');
+locationButton.on('click', function() {
+  if (!navigator.geolocation) {
+    return alert('Geolocation not supported by your browser');
+  }
+
+  navigator.geolocation.getCurrentPosition(function(position) {
+    console.log(position);
+    socket.emit('createLocationMessage', {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude
+    });
+  }, function() {
+    alert('Unable to fetch location');
+  });
+});
