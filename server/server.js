0b3400b728b2b6dd5ea8f2c7bb447aa1c0fe7b6d
pd <pd@pds-MacBook-Pro.local>
2018-01-19 23:10:36 +0800

    Send message to only people in same room

--- server/server.js
+++ server/server.js
@@ -37,13 +37,21 @@ io.on('connection', (socket) => {
 
   socket.on('createLocationMessage', (coords) => {
     console.log('coords:',coords);
-    io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
+    var user = users.getUser(socket.id);
+    
+    if (user) {
+      io.emit('newLocationMessage', generateLocationMessage(user.name, coords.latitude, coords.longitude));
+    }
   });
 
-  socket.on('createMessage', (msg, callback) => {
-    console.log('createMessage', msg);
+  socket.on('createMessage', (message, callback) => {
+    console.log('createMessage', message);
+    var user = users.getUser(socket.id);
 
-    io.emit('newMessage', generateMessage(msg.from, msg.text));
+    if (user && isRealString(message.text)) {
+      io.to(user.room).emit('newMessage', generateMessage(user.name, message.text));
+
+    }
     callback();
   });
 
